06f0cab4db1b82788825dfaf16c3e67fabcf488d
Andy Lui <andylui@Andys-MBP.home>
2017-02-27 00:41:34 -0800

    finalize scrolling remove experimentals

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 $(function() {
 	$('.carousel').slick({
 		// lazyLoad: 'ondemand',
+		infinite: true,
 		autoplay: true,
 		speed: 1000,
 		autoplaySpeed: 2000,
@@ -27,4 +28,20 @@ $(function() {
 	});
 	});
 	$(window).resize();
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      // target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      console.log(target);
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
