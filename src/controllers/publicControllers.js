ba9a9dd32a748649f0e965cabaf53d1061acdda8
Andy Lui <andylui@Andys-MBP.home>
2017-03-02 23:49:31 -0800

    data to controllers and poster sizing adjustment

--- src/controllers/publicControllers.js
+++ src/controllers/publicControllers.js
@@ -2,27 +2,27 @@ module.exports.homepage = function(req, res) {
 	res.render('index', {
 		movies: [
 		{
-			"title": "Spirited Away",
-			"poster": "http://fontmeme.com/images/USA_full-spirited-away-poster.jpg",
-			"lgPoster": "https://images4.alphacoders.com/203/203996.jpg",
-			"reviewSummary": "I first watched Spirited Away in the Sixth Grade and, even then, I knew there was something special about this movie...",
-			"reviewer": "Andy Lui"
+			title: "Spirited away",
+			poster: "http://fontmeme.com/images/USA_full-spirited-away-poster.jpg",
+			lgPoster: "https://images4.alphacoders.com/203/203996.jpg",
+			reviewSummary: "I first watched Spirited Away in the Sixth Grade and, even then, I knew there was something special about this movie...",
+			reviewer: "Andy Lui"
 
 
 		},
 		{
-			"title": "Interstellar",
-			"poster": "https://s-media-cache-ak0.pinimg.com/originals/a5/13/df/a513df413b50b1c5de6e2e98b54691d8.jpg",
-			"lgPoster": "https://blurppy.files.wordpress.com/2014/05/screen-shot-2014-05-17-at-10-06-40-pm.png",
-			"reviewSummary": "Directed by Christopher Nolan, Interstellar is a unique film that plays with time and space in a way that has never been done before in cinema. It is a science-motivated movie but also has a potent human element...",
-			"reviewer": "Andy Lui"
+			title: "Interstellar",
+			poster: "https://s-media-cache-ak0.pinimg.com/originals/a5/13/df/a513df413b50b1c5de6e2e98b54691d8.jpg",
+			lgPoster: "https://blurppy.files.wordpress.com/2014/05/screen-shot-2014-05-17-at-10-06-40-pm.png",
+			reviewSummary: "Directed by Christopher Nolan, Interstellar is a unique film that plays with time and space in a way that has never been done before in cinema. It is a science-motivated movie but also has a potent human element...",
+			reviewer: "Andy Lui"
 		},
 		{
-			"title": "Howl's Moving Castle",
-			"poster": "http://pre01.deviantart.net/47b3/th/pre/i/2016/062/0/7/spirit_of_the_demon___howl_s_moving_castle_poster_by_edwardjmoran-d9trrjf.jpg",
-			"lgPoster": "https://images7.alphacoders.com/325/325547.jpg",
-			"reviewSummary": "I watched Howl's Moving Castle a couple of years after Spirited Away, created by the same director, the masterful Hayao Miyazaki. I noticed immediately the magic imbued in the filmmaking yet there was something that distinguished it from its older sibling...",
-			"reviewer": "Andy Lui"
+			title: "Howl's Moving Castle",
+			poster: "http://pre01.deviantart.net/47b3/th/pre/i/2016/062/0/7/spirit_of_the_demon___howl_s_moving_castle_poster_by_edwardjmoran-d9trrjf.jpg",
+			lgPoster: "https://images7.alphacoders.com/325/325547.jpg",
+			reviewSummary: "I watched Howl's Moving Castle a couple of years after Spirited Away, created by the same director, the masterful Hayao Miyazaki. I noticed immediately the magic imbued in the filmmaking yet there was something that distinguished it from its older sibling...",
+			reviewer: "Andy Lui"
 		}
 		]
 	});
@@ -33,7 +33,25 @@ module.exports.login = function(req, res) {
 }
 
 module.exports.search = function(req, res) {
-	res.render('search', {});
+	res.render('search', {
+		movies: [{
+			title: "Spirited Away",
+			poster: "http://image.tmdb.org/t/p/w500/ynXoOxmDHNQ4UAy0oU6avW71HVW.jpg"
+		},
+		{
+			title: "Interstellar",
+			poster: "https://s-media-cache-ak0.pinimg.com/originals/a5/13/df/a513df413b50b1c5de6e2e98b54691d8.jpg"
+		},
+		{
+			title: "Howl's Moving Castle",
+			poster: "http://pre01.deviantart.net/47b3/th/pre/i/2016/062/0/7/spirit_of_the_demon___howl_s_moving_castle_poster_by_edwardjmoran-d9trrjf.jpg"
+		},
+				{
+			title: "Howl's Moving Castle",
+			poster: "http://pre01.deviantart.net/47b3/th/pre/i/2016/062/0/7/spirit_of_the_demon___howl_s_moving_castle_poster_by_edwardjmoran-d9trrjf.jpg"
+		}
+		]
+	});
 }
 
 module.exports.movieInfo = function(req, res) {
