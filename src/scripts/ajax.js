8d424e8e19aee578e55139f70a18496655086dd3
Andy Lui <andylui@Andys-MBP.home>
2017-03-06 00:14:49 -0800

    ajax test

--- src/scripts/ajax.js
+++ src/scripts/ajax.js
@@ -10,7 +10,7 @@ $(function() {
 				var $root = $('div.row', 'section.searchResult');
 				for (each in data.results) {
 					$section = $('<section class="desktop-c-4 tablet-c-6 mobile-c-12 postercontainer hvr-grow"></section>');
-					$link = $('<a href="movieInfo/' + data.results[each].title + '">' + data.results[each].title + '</a>');
+					$link = $('<a href="movieInfo/' + data.results[each].title + '"></a>');
 					$link.append('<img src=https://image.tmdb.org/t/p/w500' + data.results[each].poster_path + '>');
 					$section.append($link);
 					$root.append($section);
