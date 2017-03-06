68997cb67c6e0e711a86c8395a5beef5db8dec10
Andy Lui <andylui@Andys-MBP.home>
2017-03-05 23:14:04 -0800

    adjust ajax url

--- src/scripts/ajax.js
+++ src/scripts/ajax.js
@@ -1,6 +1,6 @@
 $('input[name="search"]', 'section.search').on('keyup', function() {
 	if ($(this).val().length > 3) {
-		$.getJSON('https://blooming-sea-71496.herokuapp.com/searchAPI', {
+		$.getJSON('https://cryptic-oasis-17522.herokuapp.com/searchAPI', {
 			search: $(this).val()
 		}, function(data) {
 			console.log(data.results[0].title)
