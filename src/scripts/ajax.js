cafbefae05d5091f48578ee9e09915c110066146
Andy Lui <andylui@Andys-MBP.home>
2017-03-05 23:42:40 -0800

    ajax test

--- src/scripts/ajax.js
+++ src/scripts/ajax.js
@@ -1,17 +1,18 @@
-$('input[name="search"]', 'section.search').on('keyup', function() {
-	if ($(this).val().length > 3) {
-		$.getJSON('https://cryptic-oasis-17522.herokuapp.com/searchAPI', {
-			search: $(this).val()
-		}, function(data) {
-			$root = $('div#test');
-			for (each in data.results) {
-				var $new = $('<div>hi</div>');
-				$root.append($new);
-			}
-		})
-	}
+$(function() {
+	$('input[name="search"]', 'section.search').on('keyup', function() {
+		if ($(this).val().length > 3) {
+			$.getJSON('https://cryptic-oasis-17522.herokuapp.com/searchAPI', {
+				search: $(this).val()
+			}, function(data) {
+				$root = $('div#test');
+				for (each in data.results) {
+					var $new = $('<div>hi</div>');
+					$root.append($new);
+				}
+			})
+		}
+	});
 });
-
 // section.desktop-c-4.tablet-c-6.mobile-c-12.posterContainer.hvr-grow
 //   a(href="movieInfo/"+movie.title)
 //     img(src=movie.poster)
