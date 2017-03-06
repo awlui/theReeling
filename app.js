4d6dc25d0bfdb3b2cec65d3dd0f5d94e2eff37ed
Andy Lui <andylui@Andys-MBP.home>
2017-03-06 00:32:30 -0800

    search engine working

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ var bodyParser = require('body-parser');
 var publicRoutes = require('./src/routes/publicRoutes.js');
 var privateRoutes = require('./src/routes/privateRoutes.js');
 
-var allowCrossDomain = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-};
+// var allowCrossDomain = function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// };
 
 
 var app = express();
@@ -22,15 +22,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/slick', express.static(path.join(__dirname, 'slick')));
-app.use(allowCrossDomain);
+// app.use(allowCrossDomain);
 app.use(publicRoutes);
 app.use(privateRoutes);
 
--- src/scripts/ajax.js
+++ src/scripts/ajax.js
@@ -5,10 +5,14 @@ $(function() {
 			$.getJSON('https://cryptic-oasis-17522.herokuapp.com/searchAPI', {
 				search: $(this).val()
 			}, function(data) {
+				$('div.row', 'section.searchResult').empty();
 				console.log(data);
 				var $section, $link;
 				var $root = $('div.row', 'section.searchResult');
 				for (each in data.results) {
+					if (data.results[each].poster_path === null) {
+						continue;
+					}
 					$section = $('<section class="desktop-c-4 tablet-c-6 mobile-c-12 postercontainer hvr-grow"></section>');
 					$link = $('<a href="movieInfo/' + data.results[each].title + '"></a>');
 					$link.append('<img src=https://image.tmdb.org/t/p/w500' + data.results[each].poster_path + '>');
@@ -19,6 +23,3 @@ $(function() {
 		}
 	});
 });
-// section.desktop-c-4.tablet-c-6.mobile-c-12.posterContainer.hvr-grow
-//   a(href="movieInfo/"+movie.title)
-//     img(src=movie.poster)
