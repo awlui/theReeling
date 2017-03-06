f4fab87c766c3262124fa72f9198ad74818ee097
Andy Lui <andylui@Andys-MBP.home>
2017-03-05 22:52:50 -0800

    ajax request url adjust

--- src/scripts/ajax.js
+++ src/scripts/ajax.js
@@ -1,6 +1,6 @@
 $('input[name="search"]', 'section.search').on('keyup', function() {
 	if ($(this).val().length > 3) {
-		$.getJSON('http://localhost:3000/searchAPI', {
+		$.getJSON('https://blooming-sea-71496.herokuapp.com/searchAPI', {
 			search: $(this).val()
 		}, function(data) {
 			console.log(data.results[0].title)
