f2fc90850586850e9f4996b4d78b7d53ccaa7c11
Andy Lui <andylui@Andys-MBP.home>
2017-03-09 02:56:40 -0800

    user search toggle integration end of day

--- src/scripts/ajax.js
+++ src/scripts/ajax.js
@@ -1,12 +1,9 @@
-$(function() {
-	$('input[name="search"]', 'section.search').on('keyup', function() {
-		$('div.row', 'section.searchResult').empty();
+var movieSearch = function() {
 		if ($(this).val().length > 3) {
 			$.getJSON('https://cryptic-oasis-17522.herokuapp.com/searchAPI', {
 				search: $(this).val()
 			}, function(data) {
 				$('div.row', 'section.searchResult').empty();
-				console.log(data);
 				var $section, $link;
 				var $root = $('div.row', 'section.searchResult');
 				var count = 0;
@@ -25,5 +22,41 @@ $(function() {
 				}
 			});
 		}
-	});
+};
+var userSearch = function() {
+		$.getJSON("https://blooming-sea-71496.herokuapp.com/api/user", {
+			search: $(this).val()
+		}, function(data) {
+			$('div.row', 'section.searchResult').empty();
+			var $section, $link;
+			var $root = $('div.row', 'section.searchResult');
+			var count = 0;
+			console.log(data);
+			for (each in data) {
+				console.log(data[each], "HERE");
+				(function(user) {
+					$section = $('<section class="desktop-c-4 tablet-c-6 mobile-c-12 postercontainer hvr-grow"></section>');
+					$link = $('<a href="/profile/' + data[user].id + '"></a>');
+					$link.append('<img src="/public/uploads/' + data[user].image + '">');
+					$section.append($link);
+					$root.append($section.hide().fadeIn(500*count));
+					count++;
+				})(each);
+			}
+
+		});
+		
+
+}
+$(function() {
+		$('input[name="search"]').on('keyup', movieSearch);
+		$('input[type="checkbox"]').on('change', function() {
+			if (this.checked) {
+				$('div.row', 'section.searchResult').empty();
+				$('input[name="search"]').off('keyup', movieSearch).on('keyup', userSearch).attr("placeholder", "Username");
+			} else {
+				$('div.row', 'section.searchResult').empty();
+				$('input[name="search"]').off('keyup', userSearch).on('keyup', movieSearch).attr("placeholder", "Movie");
+			}
+		});
 });
