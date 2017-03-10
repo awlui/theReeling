cf7e65ab4aadfb7825b61e4c5dbb048c3718e252
Andy Lui <andylui@Andys-MBP.home>
2017-03-09 20:05:42 -0800

    search toggle implemented pre-merge

--- src/scripts/ajax.js
+++ src/scripts/ajax.js
@@ -6,20 +6,16 @@ var movieSearch = function() {
 				$('div.row', 'section.searchResult').empty();
 				var $section, $link;
 				var $root = $('div.row', 'section.searchResult');
-				var count = 0;
-				for (each in data.results) {
-					if (data.results[each].poster_path === null) {
-						continue;
+				data.results.forEach(function(val, index) {
+					if (!val.poster_path) {
+						return;
 					}
-					(function(movie) {
-							$section = $('<section class="desktop-c-4 tablet-c-6 mobile-c-12 postercontainer hvr-grow"></section>');
-							$link = $('<a href="movieInfo/' + data.results[movie].id + '"></a>');
-							$link.append('<img src=https://image.tmdb.org/t/p/w500' + data.results[movie].poster_path + '>');
-							$section.append($link);
-							$root.append($section.hide().fadeIn(500*count));
-							count++;
-					})(each);
-				}
+					$section = $('<section class="desktop-c-4 tablet-c-6 mobile-c-12 movieSearch hvr-grow"></section>');
+					$link = $('<a href="movieInfo/' + val.id + '"></a>');
+					$link.append('<img src=https://image.tmdb.org/t/p/w500' + val.poster_path + '>');
+					$section.append($link);
+					$root.append($section.hide().fadeIn(500*index));				
+				});
 			});
 		}
 };
@@ -30,33 +26,40 @@ var userSearch = function() {
 			$('div.row', 'section.searchResult').empty();
 			var $section, $link;
 			var $root = $('div.row', 'section.searchResult');
-			var count = 0;
-			console.log(data);
-			for (each in data) {
-				console.log(data[each], "HERE");
-				(function(user) {
-					$section = $('<section class="desktop-c-4 tablet-c-6 mobile-c-12 postercontainer hvr-grow"></section>');
-					$link = $('<a href="/profile/' + data[user].id + '"></a>');
-					$link.append('<img src="/public/uploads/' + data[user].image + '">');
-					$section.append($link);
-					$root.append($section.hide().fadeIn(500*count));
-					count++;
-				})(each);
-			}
-
+			data.forEach(function(val, index) {
+				if (!val.image) {
+					console.log(val.image)
+					val.image = "http://cdn.techgyd.com/no-user-profile-picture-whatsapp.jpg";
+				} else {
+					val.image = "/public/uploads/" + val.image; 
+				}
+				$row = $('<div class="row"></div>');
+				$section = $('<section class="mobile-c-12 userSearch"></section>');
+				$imageWrapper = $('<section class="mobile-c-12 tablet-c-12 desktop-c-6  hvr-grow"></section>');
+				$biographyWrapper = $('<section class="mobile-c-12 desktop-c-6"></section>');
+				$username = $('<h2></h2>').append(val.username);
+				$biography = $('<p></p>').append(val.biography || "No Biography");
+				$biographyWrapper.append($username).append($biography);
+				$link = $('<a href="/profile/' + val.id + '"></a>');
+				$link.append('<img src=' + val.image + '>');
+				$imageWrapper.append($link);
+				$section.append($imageWrapper).append($biographyWrapper);
+				$row.append($section);
+				$root.append($row.hide().fadeIn(500*index));		
+			});
 		});
 		
 
 }
 $(function() {
-		$('input[name="search"]').on('keyup', movieSearch);
+		$('input[name="search"]').on('keyup', movieSearch).trigger('keyup');
 		$('input[type="checkbox"]').on('change', function() {
 			if (this.checked) {
 				$('div.row', 'section.searchResult').empty();
-				$('input[name="search"]').off('keyup', movieSearch).on('keyup', userSearch).attr("placeholder", "Username");
+				$('input[name="search"]').off('keyup', movieSearch).on('keyup', userSearch).attr("placeholder", "Username").trigger('keyup');
 			} else {
 				$('div.row', 'section.searchResult').empty();
-				$('input[name="search"]').off('keyup', userSearch).on('keyup', movieSearch).attr("placeholder", "Movie");
+				$('input[name="search"]').off('keyup', userSearch).on('keyup', movieSearch).attr("placeholder", "Movie").trigger('keyup');
 			}
 		});
 });
